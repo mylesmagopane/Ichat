f2627c559cb7f7c4991b320da9b213901b68b88a
Myles Magopane <mylesm@fhchamberlain.co.za>
2024-11-20 17:55:30 +0200

    2024-11-20

--- public/code.js
+++ public/code.js
@@ -1,5 +1,5 @@
 
-const socket = io('https://vercel.com/myles-projects-1e7607f0/ichat/BVxGLPnTZ7P927KPueutqy9vAcQc');
+const socket = io();
 const app = document.querySelector(".app");
 
  let uname;
